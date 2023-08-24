e1e2fbb3cab00649aab7ceef0206fd3ca4544139
Darsi Sriguru Aakash <66508746+Aakashdarsi@users.noreply.github.com>
2023-08-24 22:54:24 +0530

    NG for class added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { InputBoxComponent } from './input-box/input-box.component';
 import { FormsModule } from '@angular/forms';
 import { NgIfComponent } from './ng-if/ng-if.component';
+import { NgForComponent } from './ng-for/ng-for.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     InputBoxComponent,
-    NgIfComponent
+    NgIfComponent,
+    NgForComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ng-for/ng-for.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-for',
+  templateUrl: './ng-for.component.html',
+  styleUrls: ['./ng-for.component.css']
+})
+export class NgForComponent {
+  data = ["Mahesh", "Ram", "Krishna", "Siva"]
+  
+}
